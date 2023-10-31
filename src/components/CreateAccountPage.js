01c22861a0f4ac3a7e08104e130c2b42bc3f9333
ASiracusa <fedorafilmz@gmail.com>
2023-10-31 03:22:39 -0400

    Account Creation Page Form Validation

--- src/components/CreateAccountPage.js
+++ src/components/CreateAccountPage.js
@@ -10,7 +10,60 @@ function CreateAccountPage () {
   const [passwordConf, setPasswordConf] = useState("");
   const [isCoach, setIsCoach] = useState(false);
 
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordConfError, setPasswordConfError] = useState("");
+
   function createAccount () {
+    
+    if (firstName.length === 0) {
+      setFirstNameError("Missing first name.");
+    } else if (firstName.length > 32) {
+      setFirstNameError("First name too long.");
+    } else if (!/^[a-zA-Z]( ?[a-zA-Z]*)*$/.test(firstName)) {
+      setFirstNameError("First name disallowed format.");
+    } else {
+      setFirstNameError(null);
+    }
+
+    if (lastName.length === 0) {
+      setLastNameError("Missing last name.");
+    } else if (lastName.length > 32) {
+      setLastNameError("First name too long.");
+    } else if (!/^[a-zA-Z]( ?[a-zA-Z]*)*$/.test(lastName)) {
+      setLastNameError("First name disallowed format.");
+    } else {
+      setLastNameError(null);
+    }
+
+    if (email.length === 0) {
+      setEmailError("Missing email.");
+    } else if (email.length > 32) {
+      setEmailError("Email too long.");
+    } else if (!/^[a-zA-Z][a-zA-Z0-9]*\@[a-zA-Z][a-zA-Z0-9]*\.[a-zA-Z]{2,4}$/.test(email)) {
+      setEmailError("Incorrect email format.");
+    } else {
+      setEmailError(null);
+    }
+
+    if (password.length === 0) {
+      setPasswordError("Missing password.");
+    } else if (password.length > 32) {
+      setPasswordError("Email too long.");
+    } else if (!/^[a-zA-Z0-9!#$%&()*+,./:;<=>?@[\]^_{|}~]+$/.test(password)) {
+      setPasswordError("Incorrect password format.");
+    } else {
+      setPasswordError(null);
+    }
+
+    if (password !== passwordConf) {
+      setPasswordConfError("Passwords don't match.");
+    } else {
+      setPasswordConfError(null);
+    }
+
     // trigger call to the backend
   }
 
@@ -21,31 +74,31 @@ function CreateAccountPage () {
         <Grid item xs={12}>
           <Grid container spacing={2}>
             <Grid item>
-              <TextField id="inpFirstName" label="First Name" variant="filled" required value={firstName} onChange={(event) => {
+              <TextField id="inpFirstName" label="First Name" variant="filled" error={firstNameError} helperText={firstNameError} required value={firstName} onChange={(event) => {
                 setFirstName(event.target.value);
               }}/>
             </Grid>
             <Grid item>
-              <TextField id="inpLastName" label="Last Name" variant="filled" required value={lastName} onChange={(event) => {
+              <TextField id="inpLastName" label="Last Name" variant="filled" error={lastNameError} helperText={lastNameError} required value={lastName} onChange={(event) => {
                 setLastName(event.target.value);
               }}/>
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12}>
-          <TextField id="inpEmail" label="Email" variant="filled" required value={email} onChange={(event) => {
+          <TextField id="inpEmail" label="Email" variant="filled" error={emailError} helperText={emailError} required value={email} onChange={(event) => {
             setEmail(event.target.value);
           }}/>
         </Grid>
         <Grid item xs={12}>
           <Grid container spacing={2}>
             <Grid item>
-              <TextField id="inpPassword" label="Password" variant="filled" required type="password" value={password} onChange={(event) => {
+              <TextField id="inpPassword" label="Password" variant="filled" error={passwordError} helperText={passwordError} required type="password" value={password} onChange={(event) => {
                 setPassword(event.target.value);
               }}/>
             </Grid>
             <Grid item>
-              <TextField id="inpPasswordConf" label="Confirm Password" variant="filled" required type="password" value={passwordConf} onChange={(event) => {
+              <TextField id="inpPasswordConf" label="Confirm Password" variant="filled" error={passwordConfError} helperText={passwordConfError} required type="password" value={passwordConf} onChange={(event) => {
                 setPasswordConf(event.target.value);
               }}/>
             </Grid>
