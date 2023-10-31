3d547293f77412b49eeeae372c223d8eaeb4f079
Jumaiza <zaid.abujumaiza@gmail.com>
2023-10-31 16:59:20 -0400

    added sample app name and fixed a bit of css

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -7,7 +7,7 @@ export default function SideNav() {
 
   return (
     <div className="side-nav">
-      <h2 className="title">APP NAME</h2>
+      <h2 className="title">Fitness App</h2>
       <a className={location.pathname === '/' ? 'active' : ''} href="/">
         Home
       </a>
