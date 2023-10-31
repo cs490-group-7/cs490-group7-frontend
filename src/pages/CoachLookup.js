8e8df954e93568eba4ba83ac5c023d8d0d5417ec
Jumaiza <zaid.abujumaiza@gmail.com>
2023-10-31 12:34:00 -0400

    configured react router with sample pages

---
+++ src/pages/CoachLookup.js
@@ -0,0 +1,7 @@
+export default function CoachLookup () {
+    return (
+        <div className="coach-lookup-page">
+            <h1>Coach Lookup</h1>
+        </div>
+    )
+}
---
+++ src/pages/MyProgress.js
@@ -0,0 +1,7 @@
+export default function MyProgress () {
+    return (
+        <div className="my-progress-page">
+            <h1>My Progress</h1>
+        </div>
+    )
+}
