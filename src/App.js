589566355cd8fe04b076a4d45b1404c2f3b26c9e
Jumaiza <zaid.abujumaiza@gmail.com>
2023-11-16 15:01:13 -0500

    update title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import SideNav from './components/SideNav';
 import { Outlet } from 'react-router';
-
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'Fit Fusion';
+  }, []);
+
   return (
     <div className="App">
       <hr className='top-bar'></hr>
